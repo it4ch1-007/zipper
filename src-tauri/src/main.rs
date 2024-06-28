d5456164808fbbcfec880b6d7d0b1827f153238e
it4ch1 <akshitdavps@gmail.com>
2024-06-28 20:21:08 +0530

    added comments in main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+//Adding Rust cargo packages
 use std::fs::{File, OpenOptions};
 use std::io::{self, BufRead,Write};
 use std::{fs};
@@ -11,7 +13,7 @@ use zip::read::ZipFile;
 use tauri;
 
 
-
+//Struct to represent metadata of the zip
 #[derive(Serialize)]
 struct ZipFileMetadata<>{
   entries: usize,
@@ -20,15 +22,17 @@ struct ZipFileMetadata<>{
   is_empty: bool,
 }
 
+//This is to make password to be a global variable
 struct password{
   pswd:String,
 }
 
+//Called when the file reading does not require any password
 fn fn_without_pswd(i: usize, archive: &mut ZipArchive<File>) -> Result<ZipFile, ZipError> {
   archive.by_index(i)
 }
 
-
+//Called when password is required to decrypt the file
 fn fn_pswd(i: usize, archive: &mut ZipArchive<File>,myPassword:password) -> Result<ZipFile, ZipError> {
   match archive.by_index_decrypt(i, myPassword.pswd.as_bytes()) {
       Ok(zip_file) => Ok(zip_file),
@@ -37,6 +41,8 @@ fn fn_pswd(i: usize, archive: &mut ZipArchive<File>,myPassword:password) -> Resu
       }
   }
 }
+
+//Getting all the tauri command functions inside the invoke handler method
 fn main() {
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![config_read,read_zip_files_pswd,read_metadata,read_zip_files,extract_zip,config_write,prior_check,extract_zip_pswd])
@@ -44,6 +50,20 @@ fn main() {
     .expect("error while running tauri application");
 }
 
+//Helper function for function read_zip_files
+#[tauri::command]
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+//Another helper function for error handling
+fn fn_ret_false()->bool{
+  false
+}
+
+//To extract the zip when password is required
 #[tauri::command]
 fn extract_zip_pswd(zipPath:String,pswd:String){
   
@@ -54,7 +74,7 @@ fn extract_zip_pswd(zipPath:String,pswd:String){
 
   for i in 0..archive.len(){
     let myPassword = password{
-      pswd:pass.clone(),
+      pswd:pass.clone(), //cloning the new variable to move its value inside the loop
     };
     let mut new_archive = zip::ZipArchive::new(File::open(&zipname).unwrap()).unwrap();
       let file = match archive.by_index_decrypt(i,pass.as_bytes()){
@@ -68,20 +88,22 @@ fn extract_zip_pswd(zipPath:String,pswd:String){
               },
               
      
-      }.unwrap();
-    let substring = zipPath.rsplitn(2, '\\').nth(1).expect("msg");
+      }.unwrap(); //unwrapping to obtain a ZipFile instead of an option.
+    let substring = zipPath.rsplitn(2, '\\').nth(1).expect("Error");//getting the parent directory of Zip given
   let mut outpath = PathBuf::new();
-  let sec_str = file.enclosed_name().expect("msg");
+  let sec_str = file.enclosed_name().expect("Error");
   outpath.push(substring);
-  outpath.push(sec_str);
+  outpath.push(sec_str);//outpath for the resulting path of the new file copied or decrypted from the zip extracted
 
   if(*file.name()).ends_with('/'){
     fs::create_dir_all(&outpath).unwrap();//passing a reference as parameter
+    //to create a new directory for the files extracted
   }
   else{
 
     if let Some(p) = outpath.parent(){
-        if !p.exists(){
+        if !p.exists(){ //if the parent directory exists already then now new directory will be made
+          //without this every file will make its owbn parent directory giving a lot of duplicate directories.
            fs::create_dir_all(&p).unwrap();
         }
     }
@@ -91,79 +113,40 @@ fn extract_zip_pswd(zipPath:String,pswd:String){
   }
 }
 
-#[tauri::command]
-fn config_write(zipPath:String){
-  let path = "../src/utils/config.txt";
-  let mut config_file = path;
-  let mut vec_none: Vec<String> = vec![];
-
-  if let Ok(lines) = read_lines(config_file){
-    let mut vec_lines = Vec::new();
-        for line in lines {
-            if let Ok(utf16_line) = line {
-                let utf8_line = utf16_line.encode_utf16() 
-                    .filter(|&ch| ch != 0) 
-                    .collect::<Vec<u16>>(); 
-                let utf8_line = String::from_utf16_lossy(&utf8_line); 
-                vec_lines.push(utf8_line);
-            }
-        }
-          vec_lines.insert(0,zipPath);
-          vec_lines.pop();
-         
-          let mut output = OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .open(config_file).unwrap();
-        
-        for line in &vec_lines{
-            writeln!(output,"{}",line);
-        }
-    }
-
-}
-
-#[tauri::command]
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
-}
-
-fn fn_ret_false()->bool{
-  false
-}
-
+//Extracts the files without the need of password
 #[tauri::command]
 fn extract_zip(zippath:String,pswd:String){
   
   let zipname = std::path::Path::new(&*zippath); //getting the dereference to get the value of the string but passing it as a reference to the actual function paramter.
   let file = File::open(&zipname).unwrap();
   let mut archive = zip::ZipArchive::new(file).unwrap(); //making a new instance of the zip file
-  let mut password = "hello".to_string();
+  let mut password = "hello".to_string(); //to ensure that the empty password does not crash the app
   let mut pass = pswd;
   for i in 0..archive.len(){
-    let mut new_archive = zip::ZipArchive::new(File::open(&zipname).unwrap()).unwrap();
+    let mut new_archive = zip::ZipArchive::new(File::open(&zipname).unwrap()).unwrap(); //making a new instance of the zip file
     let myPassword = password{
-      pswd:pass.clone(),
+      pswd:pass.clone(),//cloning the new variable to move its value inside the loop
     };
       let file = match archive.by_index(i){
           Ok(mut file) => fn_without_pswd(i,&mut new_archive),
           Err(err)=> 
               {    fn_pswd(i,&mut new_archive,myPassword)},
-        }.unwrap();
-  let mut substring = zippath.rsplitn(2, '\\').nth(1).expect("msg");
+        }.unwrap();//unwrapping to obtain a ZipFile instead of an option.
+  let mut substring = zippath.rsplitn(2, '\\').nth(1).expect("Error");//getting the parent directory of Zip given
   let mut outpath = PathBuf::new();
-  let sec_str = file.enclosed_name().expect("msg");
+  let sec_str = file.enclosed_name().expect("Error");
   outpath.push(substring);
-  outpath.push(sec_str);
+  outpath.push(sec_str);//outpath for the resulting path of the new file copied or decrypted from the zip extracted
   if(*file.name()).ends_with('/'){
     fs::create_dir_all(&outpath).unwrap();//passing a reference as parameter
   }
   else{
     if let Some(p) = outpath.parent(){
         if !p.exists(){
+          //if the parent directory exists already then now new directory will be made
+          //without this every file will make its owbn parent directory giving a lot of duplicate directories.
            fs::create_dir_all(&p).unwrap();
+           //to create a new directory for the files extracted
         }
     }
   let mut outFile = fs::File::create(&outpath).unwrap();
@@ -171,6 +154,7 @@ fn extract_zip(zippath:String,pswd:String){
   }
 }
 
+//To check if the zipfile requires password to decrypt it 
 #[tauri::command]
 fn prior_check(zippath:String) -> bool{
   let zipname = std::path::Path::new(&*zippath);
@@ -183,13 +167,13 @@ fn prior_check(zippath:String) -> bool{
       match result{
           Ok(file) => {continue;},
           Err(err)=> 
-              {return fn_ret_false()},
+              {return fn_ret_false()},//Returning false in a fashinable way :P
             }
           }
-          return true;
+          return true; //the zipfile requires password and is encrypted
 }
   
-
+//To read the files inside the zip archive encrypted with the password
 #[tauri::command]
 fn read_zip_files_pswd(zippath:String,pswd:String) -> Vec<PathBuf>{
 
@@ -199,7 +183,7 @@ fn read_zip_files_pswd(zippath:String,pswd:String) -> Vec<PathBuf>{
   let mut archive = zip::ZipArchive::new(file).unwrap();
   for i in 0..archive.len(){
     let mut new_archive = zip::ZipArchive::new(File::open(&zipname).unwrap()).unwrap();
-      let file =  archive.by_index_decrypt(i,pswd.as_bytes()).unwrap();
+      let file =  archive.by_index_decrypt(i,pswd.as_bytes()).unwrap(); //to decrypt the files inside the zip one by one using the password from the prompt
       let outpath = match file.enclosed_name(){ //This resolves a security issue as here it checks whether the path is trying to get out of the directory or not
 
         Some(path) => path.to_owned(), //borrowing the instance of the filepath
@@ -207,18 +191,20 @@ fn read_zip_files_pswd(zippath:String,pswd:String) -> Vec<PathBuf>{
       };
       return_vec.push(outpath);
 }
-  return_vec
+  return_vec //Returning tjhe vector containing the zip files name
 }
 
+//Function to read the metadata of the zip file central directory
 #[tauri::command]
 fn read_metadata(archive: String) -> String{
       let file = File::open(archive).unwrap();
-      let mut zip_archive = ZipArchive::new(file).unwrap();
-      let num_entries = zip_archive.len();
-      let comment = std::str::from_utf8(zip_archive.comment()).unwrap().to_string();
-      let prepended_data_size = zip_archive.offset();
-      let is_empty = zip_archive.is_empty();
+      let mut zip_archive = ZipArchive::new(file).unwrap(); //making the file as a type of ZipArchive
+      let num_entries = zip_archive.len(); //number of entries inside the zip
+      let comment = std::str::from_utf8(zip_archive.comment()).unwrap().to_string(); //the comment stored inside the zip central directory
+      let prepended_data_size = zip_archive.offset(); //The prepended data inside the bytes representation of the zip file
+      let is_empty = zip_archive.is_empty();//If the zip archive is empty or not
      
+     //Declaring an instance of the type ZipFileMetadata to store the details of the zip.
       let zip_metadata = ZipFileMetadata{
         entries: num_entries,
         comment:comment,
@@ -226,7 +212,7 @@ fn read_metadata(archive: String) -> String{
         is_empty: is_empty,
       };
 
-      serde_json::to_string(&zip_metadata).unwrap()
+      serde_json::to_string(&zip_metadata).unwrap() //return the metadata as a json string
 }
 
 #[tauri::command]
@@ -263,11 +249,12 @@ fn read_zip_files(zippath:String,pswd:String) -> Vec<PathBuf>{
   return_vec
 }
 
+//Function to read the configuration file to represent what files are viewed recently
 #[tauri::command]
 fn config_read() -> Vec<String>{
-  let path = "../src/utils/config.txt";
+  let path = "../src/utils/config.txt";//Path of the configuiration file
   let mut config_file = path;
-  let mut vec_none: Vec<String> = vec![];
+  let mut vec_none: Vec<String> = vec![]; //to return a none vector if there is any error in reading the file
 
   if let Ok(lines) = read_lines(config_file){
     let mut vec_lines = Vec::new();
@@ -284,7 +271,7 @@ fn config_read() -> Vec<String>{
                 
             }
         }
-        if vec_lines.len()>=5 {
+        if vec_lines.len()>=5 { //Only represent the last 5 zip files that were extracted using Zipper.
         vec_lines[0..5].to_vec()
         }
         else{
@@ -300,3 +287,39 @@ fn config_read() -> Vec<String>{
   
 }
 
+//Function writing the recent extracted files
+#[tauri::command]
+fn config_write(zipPath:String){
+  let path = "../src/utils/config.txt";//this is the configuration file storing the recent extracted files 
+  //IF ANYONE WANTS TO RUN THE BUILD VERSION THEN THE CONFIG.TXT FILE MUST BE PLACED WHERE ITS PATH IS CONFIGURED FOR THAT IS INSIDE THE SAM DIRECTORY AS THE EXECUTABLE FILE.
+
+  let mut config_file = path;
+  let mut vec_none: Vec<String> = vec![];
+
+  if let Ok(lines) = read_lines(config_file){
+    let mut vec_lines = Vec::new();
+        for line in lines {
+            if let Ok(utf16_line) = line {
+                let utf8_line = utf16_line.encode_utf16() 
+                    .filter(|&ch| ch != 0) 
+                    .collect::<Vec<u16>>(); 
+                let utf8_line = String::from_utf16_lossy(&utf8_line); //Converting hex representation back to Ascii String
+                vec_lines.push(utf8_line);
+            }
+        }
+          vec_lines.insert(0,zipPath);
+          vec_lines.pop();
+         
+
+         //Truncating the whole file and writing to it again
+          let mut output = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .open(config_file).unwrap();
+        
+        for line in &vec_lines{
+            writeln!(output,"{}",line);
+        }
+    }
+
+}
